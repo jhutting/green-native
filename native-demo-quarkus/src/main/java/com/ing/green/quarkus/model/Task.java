e0e8e69d2a4e1f525f3e30f38e374b7491781335
Johan Hutting <9496809+jhutting@users.noreply.github.com>
2025-05-22 22:47:37 +0200

    layout Task record

--- native-demo-quarkus/src/main/java/com/ing/green/quarkus/model/Task.java
+++ native-demo-quarkus/src/main/java/com/ing/green/quarkus/model/Task.java
@@ -1,3 +1,7 @@
 package com.ing.green.quarkus.model;
 
-public record Task(Long id, String title, String assignedTo, TaskStatus status) { }
+public record Task(
+        Long id,
+        String title,
+        String assignedTo,
+        TaskStatus status) { }
